b97ad62debeaf55d8e436eefbcfc4303b17304e3
GodsonO <godsonomoghene15@gmail.com>
2024-01-03 12:51:27 +0000

    promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,9 +2,9 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
   return Promise.allSettled([
-    promises
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
   ]).then((values) => values.map((value) => ({
     status: value.status,
     value: value.status === 'fulfilled' ? value.value : String(value.reason),
